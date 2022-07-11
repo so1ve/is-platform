72da36fe54358d7364b72dfcf8fd6e88edb9d349
so1ve <nn_201312@163.com>
2022-07-11 19:01:39 +0800

    init

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# @so1ve/is-browser
+# is-platform
 
-[![NPM version](https://img.shields.io/npm/v/@so1ve/is-browser?color=a1b858&label=)](https://www.npmjs.com/package/@so1ve/is-browser)
+[![NPM version](https://img.shields.io/npm/v/is-platform?color=a1b858&label=)](https://www.npmjs.com/package/is-platform)
 
 ## License
 
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 export function isBrowser () {
   return typeof window === "undefined";
 }
+
+export function isServer () {
+  return !isBrowser();
+}
+
+export function isNode () {
+  return typeof process === "undefined";
+}
+
+export function isDeno () {
+  // @ts-expect-error That's OK
+  return typeof Deno === "undefined";
+}
